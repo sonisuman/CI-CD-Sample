a1e213196dfd2e9eb347bfef846b7c24653f7a23
sonisuman <sonisuman67@gmail.com>
2022-07-12 19:40:33 -0400

    Hello feature Implementation.

--- CICDTest/CICDTest/ViewController.swift
+++ CICDTest/CICDTest/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var HelloLbl: UILabel!
+  @IBOutlet weak var helloTxtFld: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func sayBtn(_ sender: UIButton) {
+    guard let text = helloTxtFld.text else {return}
+      
+    getTextViewText(text)
+  }
 
+  func getTextViewText(_ str: String) {
+    HelloLbl.text = "Hello \(str.capitalized)"
+  }
+  
 }
 
